580d0eb2f25fbe6f3915747d9a9b313efa9de946
tayarndt <Taylor.e.arndt@wmich.edu>
2020-03-31 18:22:26 -0400

    added better user validation and changed cases on the message prompts and variables

--- simpleToDo.py
+++ simpleToDo.py
@@ -1,22 +1,23 @@
 
 while True:
 	file=open("items.txt","r")
-	message=input("what would you like to do? Press R for reading your list, e to exit the program, h to get some help, or A to add an item to your list")
+	message = input("what would you like to do? Press R for reading your list, E to exit the program, H to get some help, or A to add an item to your list")
+	message = message.upper()
 
-	if message=="e":
-		print("exiting simple todo")
+	if message=="E":
 		break
 
-	if message == "h":
+	if message == "H":
 		print("this is the message field. Type either R which will read your todo items, A which will add an item, or the number 1 to exit")
 
-	if message=="R":
+	if message== "R":
 		list=file.readlines()
 		print(list)
 	file.close()
 
 	file=open("items.txt","a")
-	if message == "a":
+
+	if message == "A":
 		while True:
 			name=input("enter item name:")
 

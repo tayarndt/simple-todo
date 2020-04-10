42891eb3de27b31efa8d6377553a151ba0ca37c8
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-10 17:37:44 -0400

    added way to create file if user doesn't have the item.txt file on there system.

--- simpleToDo.py
+++ simpleToDo.py
@@ -1,29 +1,38 @@
+import os.path
+from os import path
+def main():
+	while True:
+		if not (path.exists("items.txt")):
+			print("it apears you don't have a text file for your items to be stored in. We will create it now. ")
+			print("file created successfully. You may now use the program.")
+			file = open("items.txt","a")
+		else:
 
-while True:
-	file=open("items.txt","r")
-	message = input("what would you like to do? Press R for reading your list, E to exit the program, H to get some help, or A to add an item to your list")
-	message = message.upper()
+			file=open("items.txt","r")
+			message = input("what would you like to do? Press R for reading your list, E to exit the program, H to get some help, or A to add an item to your list")
+			message = message.upper()
 
-	if message=="E":
-		break
-
-	if message == "H":
-		print("this is the message field. Type either R which will read your todo items, A which will add an item, or the number 1 to exit")
-
-	if message== "R":
-		list=file.readlines()
-		print(list)
-	file.close()
-
-	file=open("items.txt","a")
-
-	if message == "A":
-		while True:
-			name=input("enter item name:")
-
-			if name !=  "1":
-				file.write(name + "\n")
-			else:
+			if message=="E":
 				break
 
-file.close()
+			if message == "H":
+				print("this is the message field. Type either R which will read your todo items, A which will add an item, or the number 1 to exit")
+
+			if message== "R":
+				list=file.readlines()
+				print(list)
+			file.close()
+
+			file=open("items.txt","a")
+
+			if message == "A":
+				while True:
+					name=input("enter item name:")
+
+				if name !=  "1":
+					file.write(name + "\n")
+				else:
+					break
+
+			file.close()
+main()

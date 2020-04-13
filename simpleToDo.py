d640fb1e31ff0696bba6ae800c61278621df170e
tayarndt <Taylor.e.arndt@wmich.edu>
2020-04-13 17:14:48 -0400

    restructured code

--- simpleToDo.py
+++ simpleToDo.py
@@ -1,38 +1,55 @@
 import os.path
 from os import path
+
 def main():
+	welcome()
+
+
+def welcome():
+	if  (path.exists("items.txt")):
+		file =  open("items.txt","a")
+		menu()
+	else:
+		file =  open("items.txt","a")
+		print("it appears you don't have a text file for your items to be stored in. We will create it now. ")
+		print("file created successfully. You may now use the program.")
+		file.close()
+		return file
+
+
+def menu():
 	while True:
-		if not (path.exists("items.txt")):
-			print("it apears you don't have a text file for your items to be stored in. We will create it now. ")
-			print("file created successfully. You may now use the program.")
-			file = open("items.txt","a")
+		message = input("what would you like to do? Type A to add a task, E to exit,H to get some help, R to read your list")
+		if message.upper() == "H":
+			print("Welcome to the help. Type A to add tasks, R to read your tasks, or E to exit this application.")
+		elif message.upper() == "E":
+			break
+		elif message.upper() =="R":
+			readFile()
+		elif message.upper() == "A":
+			writeFile()
+
+
+def readFile ():
+	fileToRead = open("items.txt","r")
+	for line in fileToRead:
+		line = fileToRead.readlines()
+		print(line)
+		return fileToRead
+		fileToRead.close()
+
+
+def writeFile():
+	fileToWrite = open("items.txt","a")
+	while True:
+		task = input("task name:")
+
+		if task != "1":
+			fileToWrite.write(task)
+			fileToWrite.write("\n")
 		else:
+			break
+			return fileToWrite
+			fileToWrite.close()
 
-			file=open("items.txt","r")
-			message = input("what would you like to do? Press R for reading your list, E to exit the program, H to get some help, or A to add an item to your list")
-			message = message.upper()
-
-			if message=="E":
-				break
-
-			if message == "H":
-				print("this is the message field. Type either R which will read your todo items, A which will add an item, or the number 1 to exit")
-
-			if message== "R":
-				list=file.readlines()
-				print(list)
-			file.close()
-
-			file=open("items.txt","a")
-
-			if message == "A":
-				while True:
-					name=input("enter item name:")
-
-				if name !=  "1":
-					file.write(name + "\n")
-				else:
-					break
-
-			file.close()
 main()
